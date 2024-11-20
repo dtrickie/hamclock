eb6ec4f5723e082c2f8561f9dc61ed889bd9a231
dtrickie <dtrickie@gmail.com>
2024-11-20 15:03:19 -0500

    hamclock.sh

--- hamclock.sh
+++ hamclock.sh
@@ -1,5 +1,5 @@
 sudo apt-get update -y
-sudo apt-get -y install curl make g++ libx11-dev libgpiod-dev xdg-utils
+sudo apt-get install curl make g++ libx11-dev libgpiod-dev xdg-utils -y
 sudo apt install unzip -y
 cd
 rm -fr ESPHamClock
@@ -8,5 +8,4 @@ unzip ESPHamClock.zip
 cd ESPHamClock
 make -j 4 hamclock-800x480
 sudo make install
-
 hamclock &
